768db1b356329ca189fb9e96e88a0c21da3739df
anharID <63109799+anharID@users.noreply.github.com>
2022-11-02 23:42:04 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Counter from "./learn-useState/counter";
 import Effect from "./learn-useEffect/index";
 import Context from "./learn-useContext/index";
+import NewHook from "./TugasNewHook";
 
 function App() {
   return (
@@ -21,12 +22,16 @@ function App() {
             <li className="li">
               <Link to="/context">Use Context</Link>
             </li>
+            <li className="li">
+              <Link to="/ref">Use Ref</Link>
+            </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" exact element={<Counter />} />
           <Route path="/effect" exact element={<Effect />} />
           <Route path="/context" exact element={<Context />} />
+          <Route path="/ref" exact element={<NewHook />} />
         </Routes>
       </div>
     </Router>
